0edb59369bbb09d31249e07e109bc7c0a7052029
안민 <iededg@naver.com>
2023-02-03 23:51:07 +0900

    2023.02.02 success connect database

--- src/Book/test.java
+++ src/Book/test.java
@@ -8,14 +8,14 @@ public class test {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/jdbc";
         String userName = "root";
-        String password = "비밀번호";
+        String password = "0000";
 
         Connection connection = DriverManager.getConnection(url, userName, password);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select * from users");
 
         resultSet.next();
-        String name = resultSet.getString("name");
+        String name = resultSet.getString("id");
         System.out.println(name);
 
         resultSet.close();
